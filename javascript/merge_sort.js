cd23c2cbabf0004a2e6da2134d8beb1c44bf1135
Scott Schwab <scottschwab86@gmail.com>
2024-02-11 15:34:32 -0600

    Complete challenge

--- javascript/merge_sort.js
+++ javascript/merge_sort.js
@@ -1,9 +1,32 @@
 function merge(arr1, arr2) {
   // type your code here
+  let mergedArr = [];
+  while (arr1.length > 0 && arr2.length > 0) {
+    if (arr1[0] <= arr2[0]) {
+      mergedArr.push(arr1[0]);
+      arr1 = arr1.slice(1);
+    } else {
+      mergedArr.push(arr2[0]);
+      arr2 = arr2.slice(1);
+    }
+  }
+  mergedArr = mergedArr.concat(arr1);
+  mergedArr = mergedArr.concat(arr2);
+  return mergedArr;
 }
 
 function mergeSort(arr) {
   // type your code here
+  if (arr.length < 2) {
+    return arr;
+  }
+
+  let middle = Math.floor(arr.length / 2)
+
+  let left = mergeSort(arr.slice(0, middle));
+  let right = mergeSort(arr.slice(middle, arr.length));
+
+  return merge(left, right)
 }
 
 if (require.main === module) {
